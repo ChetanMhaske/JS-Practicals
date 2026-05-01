22f8ab0aee3135c3e8c0038b9f78d25a277e7aa5
ChetanMhaske <chetanmhaske451@gmail.com>
2026-05-01 13:41:11 +0530

    first commit - practical 1 and demo

---
+++ JS-Practicals/Core JS/1PR.js
@@ -0,0 +1,104 @@
+console.log("Hello, World! This is 1PR.js");
+console.log("Hello, World! This is 1PR.js");
+console.log("Hello, World! This is 1PR.js");
+console.log("Hello, World! This is 1PR.js");
+console.log("Hello, World! This is 1PR.js");
+console.log("Hello, World! This is 1PR.js");
+console.log("Hello, World! This is 1PR.js");
+
+//Declaration//        ////Initialization////
+let      fruits     =    "Apple, Banana, Cherry";
+
+
+//variable is not declared but initialized
+animalname="tiger"; 
+console.log(animalname)
+
+/*
+variable
+must start with letter, underscore, or $ sign
+cannot start with number
+cannot use JS reserved keywords
+its case-sensitive
+*/
+
+
+
+let num=18;
+num=20
+num=21
+num=22
+num=num-1;
+num=num+1; 
+// num++/num+=1 or num--/num-=1 can also be used for incrementing or decrementing by 1 
+
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+//number datatype
+let x=10;
+let y=3.14;
+let z=-5;
+let longNum=10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000n; //for big integers we can use 'n' at the end
+let w=1e3; //1e3 means 1*10^3 = 1000
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(longNum)
+console.log(w);
+
+
+
+
+
+//string datatype
+let a='apple';
+let b="banana";
+let c=`cherry`;
+let d=`${b}`; //backticks + string interpolation (${expression}) can be used for accessing variables inside strings
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+//task print :my name is <your name> and my age is <your age>
+let name1="chetan";
+let age=11/0;
+console.log('My name is ' + name1 + ' and my age is ' + age);
+console.log(`My name is ${name1} and my age is ${age}`)
+
+
+
+
+
+
+
+//boolean datatype
+let flag1=true;
+let flag2=false;
+console.log(flag1);
+console.log(flag2);
+
+let userAge = 16
+let voteFlag = userAge >= 18;
+console.log(voteFlag); // Output: false
+
+
+
+
+
+
+console.log(typeof longNum)
+console.log(typeof a)
+console.log(typeof voteFlag)
+
+
---
+++ JS-Practicals/Demo.js
@@ -0,0 +1,42 @@
+// let personObj = {
+//     name: "sourav",
+//     age: 20,
+//     gender: "male",
+//     key: "value"
+// }
+// console.log(Object.keys(personObj))//gives an array of keys
+// console.log(Object.keys(personObj).length)//gives length of object
+// console.log(Object.values(personObj))//gives array values
+
+
+// //question .
+
+// // let a=Object.values(personObj)
+// // function objDemo(a){
+
+// //     console.log(a)
+
+// //     for(let i=0;i<a.length;i++){
+// //         personObj[a[i]]
+// //     }
+// // }
+// // console.log(objDemo(a))
+
+// let keys = Object.keys(personObj);
+// console.log(keys)
+// for (let i = 0; i < keys.length; i++) {
+//     personObj[keys[i]]
+// }
+
+
+let arr = [[1,2,3],[4,5,6],[7,8,9]]
+console.log(arr[0][0])//1
+console.log(arr[0][1])//2
+console.log(arr[0][2])//3
+console.log(arr[1][0])//4
+console.log(arr[1][1])//5
+console.log(arr[1][2])//6
+console.log(arr[2][0])//7
+console.log(arr[2][1])//8
+console.log(arr[2][2])//9   
+
