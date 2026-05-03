290a3a35daee2c32f8fc915d17c6397c0e8ab57d
ChetanMhaske <chetanmhaske451@gmail.com>
2026-05-03 17:47:37 +0530

    switch case practical

---
+++ JS-Practicals/5PR_switch.js
@@ -0,0 +1,101 @@
+// let week = 'mon'
+// let week = 'tue'
+// let week = 'wed'
+// let week = 'thu'
+// let week = 'fri'
+// let week = 'sat'
+// let week = 'sun'
+
+// if (week === 'mon') {
+//     console.log('Monday')
+// }
+// else if (week === 'tue') {
+//     console.log('Tuesday')
+// }
+// else if (week === 'wed') {
+//     console.log('Wednesday')
+// }
+
+// switch (week){
+//     case 'mon':
+//         console.log('Monday')
+//         break
+//     case 'tue':
+//         console.log('Tuesday')
+//         break
+//     case 'wed':
+//         console.log('Wednesday')
+//         break
+//     case 'thu':
+//         console.log('Thursday')
+//         break
+//     case 'fri':
+//         console.log('Friday')
+//         break
+//     case 'sat':
+//         console.log('Saturday')
+//         break
+//     case 'sun':
+//         console.log('Sunday')
+//         break
+//     default:
+//         console.log('Invalid')
+//         break
+// }
+
+// let a = null
+// switch(a){
+//     case 3:
+//         console.log('Too small')
+//         break
+//     case 4:
+//         console.log('Correct answer')
+//         break
+//     case 5:
+//         console.log('Too big')
+//         break
+//     default:
+//         console.log('I don\'t know such values')
+//         break
+// }
+
+/*
+90-100 a+
+80-89 a
+70-79 b
+60-69 c
+50-59 d
+40-49 e
+0-39 f
+*/
+// let score = 25
+// if(score >= 90 && score <= 100){
+//     console.log('A+')
+// }
+// else if(score >= 80 && score <= 89){
+//     console.log('A')
+// }               
+// else if(score >= 70 && score <= 79){
+//     console.log('B')
+// }
+// else if(score >= 60 && score <= 69){
+//     console.log('C')
+// }
+// else if(score >= 50 && score <= 59){
+//     console.log('D')
+// }   
+// else if(score >= 40 && score <= 49){
+//     console.log('E')
+// }
+// else{
+//     console.log('F')
+// }
+
+
+// let a = score >= 90 && score <=100 ? 'A+' :
+//         score >= 80 && score <=89 ? 'A' :
+//         score >= 70 && score <=79 ? 'B' :   
+//         score >= 60 && score <=69 ? 'C' :
+//         score >= 50 && score <=59 ? 'D' :
+//         score >= 40 && score <=49 ? 'E' : 'F'
+// console.log(a) 
