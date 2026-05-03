0500e4aa02124c39224fd5eb8a76eff19f9a0abd
ChetanMhaske <chetanmhaske451@gmail.com>
2026-05-03 14:51:20 +0530

    logical operators practice

---
+++ JS-Practicals/4PR_LogicalOps.js
@@ -0,0 +1,65 @@
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// let a = true 
+// let b = false
+// let res = a || b
+// console.log(res)
+
+// let hour = 11
+// let isWeekend = false
+// if(hour < 10 || hour > 18 || isWeekend){
+//     console.log("The office is closed.")
+// }
+
+// let a = 0, b = false, c = 0
+
+// let res = a || b || c
+// console.log(res)
+
+// console.log(1 || 0)
+// console.log(null || 2)
+// console.log(null || 0 || 1)
+// console.log(null || undefined || 0)
+
+// let fn = ''
+// let ln = ''
+// let un = ''
+
+// let res = fn || ln || un || 'Anonymous'
+// console.log(res)
+
+// console.log(true || (console.log("not printed")))
+// console.log(false || (console.log("printed")))
+
+//////////////////////////AND Operator//////////////////////////
+
+// console.log(true && true)
+// console.log(true && false)
+// console.log(false && true)
+// console.log(false && false)
+
+//3 yor are given 3 numbers a,b,c now you have to print the greatest number among them
+
+// let a = 10, b = 20, c = 15
+// if(a > b && a > c){
+//     console.log("a is greatest")
+// }else if(b > a && b > c){
+//     console.log("b is greatest")
+// }       
+// else{
+//     console.log("c is greatest")
+
+// }
+
+// let a = true, b = 0, c = false
+// let res = a && b && c
+// console.log(res)
+
+
+console.log(!true)
+console.log(!false)
+console.log(!0)
+console.log(!3)
---
+++ JS-Practicals/4PR_LogicalOps2.js
@@ -0,0 +1,30 @@
+// let a 
+// let b = 10
+// console.log(a ?? b)
+
+// let a = null
+// let b // undefined
+// console.log(a ?? b)
+
+// let fn
+// let un 
+// let ln
+
+// console.log(fn ?? ln ?? un ?? "abc")
+
+
+// let height = false
+
+// console.log(height || 100)
+// console.log(height ?? 100)
+
+// let height = 10
+// let width
+// let area = (height ?? 100) * (width ?? 100)
+// console.log(area)
+
+// drawbacks of it
+// let x = 1 && 2 ?? 3 //syntax error
+// let x = (1 && 2) ?? 3 // works fine
+// console.log(x)
+
