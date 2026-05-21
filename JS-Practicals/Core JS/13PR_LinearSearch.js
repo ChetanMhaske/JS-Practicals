d8b4b9186c22199d5ae80bbb15900c110a2f86c7
ChetanMhaske <chetanmhaske451@gmail.com>
2026-05-21 10:45:24 +0530

    linear search

---
+++ JS-Practicals/Core JS/13PR_LinearSearch.js
@@ -0,0 +1,67 @@
+/*
+
+create a function search that takes an array and a target as input
+task is to print yes if element exists in array
+and no if element is not in array
+
+*/
+
+// let arr = [1,2,3,4,5]
+// let target = 4
+
+// function LinearSearch(arr,target){
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] == target){
+//             return "Yes"
+//         }
+//     }
+//     return "No"
+// }
+// console.log(LinearSearch(arr,target))
+
+
+
+
+/*
+
+make an countOccurance function that takes an array and target as 
+input and returns the no of occurances of that elements
+
+*/
+
+
+// let arr = [4,1,4,3,4,5]
+// let target = 4
+// function countOccurance(arr,target){
+//     let count = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] === target){
+//             count++
+//         }
+//     }
+//     return count
+// }
+// console.log(countOccurance(arr,target))
+
+
+
+
+
+/*
+
+make findMax function that takes an array as input and returns
+the largest element in array
+
+*/
+
+// let arr = [1,2,6,4,5]
+// function findMax(arr){
+//     let max = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] > max){
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+// console.log(findMax(arr))
